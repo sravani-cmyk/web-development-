ab634e8d811905d26722ac086e3599c48cc49deb
sravani-cmyk <sravanikandukuri76@gmail.com>
2024-08-31 08:41:09 +0530

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,17 @@
+// var generateName = require("sillyname");
+// import generateName from "sillyname"
+// var sillyname = generateName();
+// console.log('my name is: ${SillyName}.' );
+
+
+
+// import sillyname from 'sillyname';
+// const SillyName = sillyname();
+// console.log('your name is: ${SillyName}');
+
+
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log('i am ${name}!');
