791b7b7e94e78d6c5e21de1d5e102a4f315beab8
sravani-cmyk <sravanikandukuri76@gmail.com>
2024-09-22 07:57:42 +0530

    Add files via upload

---
+++ Keeper appc-1/src/293-keeper-app-part-1-challenge/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return;
+  <p>Copyright © {currentYear} 2024 </p>;
+}
+
+export default Footer;
---
+++ Keeper appc-1/src/293-keeper-app-part-1-challenge/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note() {
+  return (
+    <div>
+      <h1>This is Title </h1>
+      <p>This is the content </p>
+    </div>
+  );
+}
+
+export default Note;
